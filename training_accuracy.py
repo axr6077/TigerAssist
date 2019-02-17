207a4ba2578ad7765e4b263a6f474790e87a9700
axr6077 <36084526+axr6077@users.noreply.github.com>
2019-02-17 02:21:45 -0500

    fix an issue and test accuracy of the neural network

--- training_accuracy.py
+++ training_accuracy.py
@@ -4,33 +4,11 @@
 import cv2
 import numpy as np
 from numpy.linalg import norm
+# from NeuralNet_train import SVM
 
 svm_params = dict(kernel_type = cv2.ml.SVM_RBF,
                   svm_type = cv2.ml.SVM_C_SVC,
                   C= 2.67, gamma = 5.383)
-
-class StatModel(object):
-    def load(self, fn):
-        self.model.load(fn)
-    def save(self, fn):
-        self.model.save(fn)
-
-class SVM(StatModel):
-    def __init__(self, C = 1, gamma = 0.5):
-        self.model = cv2.ml.SVM_create()
-        self.model.setGamma(gamma)
-        self.model.setC(C)
-        self.model.setKernel(cv2.ml.SVM_RBF)
-        self.model.setType(cv2.ml.SVM_C_SVC)
-
-    def train(self, samples, responses):
-        print("Number of Sample = " + str(len(samples)))
-        print("Number of responses = " + str(len(responses)))
-        self.model.train(samples, cv2.ml.ROW_SAMPLE, responses)
-
-    def predict(self, samples):
-        results = np.array(self.model.predict(samples)). ravel()
-        return results
 
 def preprocess_hog(digits):
     samples = []
@@ -41,12 +19,13 @@ def preprocess_hog(digits):
             gx, gy
         )
         bin_n = 16
-        bin = np.int32(bin_n*ang / 2*np.pi)
+        bin = np.int32(bin_n * ang / 2 * np.pi)
         bin_cells = bin[:100, :100], bin[100:, :100], bin[:100, 100:], bin[100:, 100:]
         mag_cells = mag[:100, :100], mag[100:, :100], mag[:100, 100:], mag[100:, 100:]
         hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
         hist = np.hstack(hists)
 
+        #hellinger kernel
         eps = 1e-7
         hist /= hist.sum() + eps
         hist = np.sqrt(hist)
@@ -74,23 +53,44 @@ def hog_single(img):
 
 def NeuralNet_train(num):
     imgs = []
-    for i in range (65, num + 65):
-        for j in range(1, 401):
-            print('Class ' + chr(i) + ' is being loaded ')
-            imgs.append(cv2.imread('TrainData/' + chr(i) + '_' + str(j) + '.jpg', 0)) #store images in a list
-        labels = np.repeat(np.arange(1, num+1), 400)
-        samples = preprocess_hog(imgs)
-        print('SVM is building')
-        #print('got here')
-        print(len(labels))
-        print(len(samples))
-        model = SVM(C = 2.97, gamma = 5.383)
-        model.train(np.array(samples), np.array(labels))
+    for i in range(65, num + 65):
+        for j in range(1, 91):
+            print("loading TrainData/" + chr(i) + "_" + str(j) + ".jpg")
+            imgs.append(cv2.imread('TrainData/' + chr(i) + '_' + str(j) + '.jpg', 0))
+    labels = np.repeat(np.arange(1, num + 1), 90)
+    samples = preprocess_hog(imgs)
+    print('training with support vector machine')
+    print(len(labels))
+    print(len(samples))
+    model = cv2.SVM()
+    model.train(samples, labels, params = svm_params)
     return model
 
-def predict(model, img):
-    samples = hog_single(img)
-    resp = model.predict(samples)
-    return resp
+def testNeuralNet_train(num):
+    imgs = []
+    for i in range(65, num + 65):
+        for j in range(91, 101):
+            print("loading TestData/" + chr(i) + '_' + str(j) + '.jpg')
+            imgs.append(cv2.imread('TrainData/' + chr(i) + '_' + str(j) + '.jpg', 0))
+    labels_testing = np.repeat(np.arange(1, num+1), 10)
+    print("Testing Neural Network")
+    print(len(labels_testing))
+    print(len(imgs))
+    return imgs, labels_testing
 
+model = NeuralNet_train(4)
+
+test_imgs, test_labels = testNeuralNet_train(4)
+count = 0.0
+k = 0
+
+for i in test_imgs:
+    test_sample = hog_single(i)
+    resp = model.predict_all(test_sample).ravel()
+    #print(int)(resp[0])
+    if test_labels[k] == (int)(resp[0]):
+        count += 1.0
+    k += 1
+
+print("Accuracy= ", (count/k)*100, "%")
 #EOF
