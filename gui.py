83a647758a532473c6b814ee3b45c639868cdf8d
axr6077 <36084526+axr6077@users.noreply.github.com>
2019-02-17 05:49:35 -0500

    completed frontend code

--- gui.py
+++ gui.py
@@ -9,3 +9,69 @@ class Ctrl:
         self.root = root
         self.view = View(root)
         self.model = Model(self)
+        self.view.setSubtitle("TigerAssist")
+        listener = assistListerner()
+        controller = Leap.Controller()
+        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE)
+        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE)
+        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP)
+        controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP)
+
+        listener.addUtils(self.model)
+        controller.add_listener(listener)
+
+        self.root.mainloop()
+        print("Press enter to quit!")
+        try:
+            sys.stdin.readline()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            controller.remove_listener(listener)
+
+        def gotUpdates(self):
+            self.view.setSubtitle(self.model.getText())
+
+class Model():
+
+    def __int__(self, ctrl):
+        self.ctrl = ctrl
+        self.txt = "TigerAssist"
+
+    def getText(self):
+        return self.txt
+
+    def textChanged(self, text):
+        if (len(self.txt)) < 50:
+            self.txt = self.txt + text
+        else:
+            self.txt = text
+        self.ctrl.gotUpdates()
+
+    def clearText(self):
+        self.txt = ''
+        self.ctrl.gotUpdates()
+
+class View:
+    def loadView(self):
+        self.subtitle = Label (self.ctrl, font = ('Helvetica', '36'), fg = 'gray30', bg = 'gray32', anchor = S, pady = 50)
+        self.subtitle.master.wm_attributes("-topmost", True)
+        self.subtitle.master.wm_attributes("-transparentcolor", "gray32")
+        self.subtitle.pack(fill = BOTH, expand = 1, side = BOTTOM)
+
+    def __init__(self, ctrl):
+        self.ctrl = ctrl
+        self.loadView()
+
+    def setSubtitle(self, txt):
+        self.subtitle.configure(text = txt)
+
+def go():
+    root = Tk()
+    root.title('TigerAssist')
+    root.attributes("-fullscreen", True)
+    app = Ctrl(root)
+
+#EOF
+
+
