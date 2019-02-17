28c0de069b18e1e5f53bf3f0f23d38534fff4644
axr6077 <36084526+axr6077@users.noreply.github.com>
2019-02-17 02:55:59 -0500

    fixed two issues with util source

--- util.py
+++ util.py
@@ -3,9 +3,9 @@
 '''
 import cv2
 import numpy as np
-import NeuralNet_train as svm
+import NeuralNet_train as networkTrain
 
-def MaxContour(contours, min_area = 200):
+def getMaxContour(contours, min_area = 200):
     maxC =  None
     MaxArea = min_area
     for cnt in contours:
@@ -14,3 +14,15 @@ def MaxContour(contours, min_area = 200):
             maxC = cnt
     return maxC
 
+def getGestureImg(cnt, img, th, model):
+    x, y, w, h = cv2.boundingRect(cnt)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    imgT = img[y:y+h, x:x+w]
+    imgT = cv2.bitwise_and(imgT, imgT, mask = th[y:y+h, x:x+w])
+    imgT = cv2.resize(imgT, (200, 200))
+    imgTG = cv2.cvtColor(imgT, cv2.COLOR_BGR2YCR_CB)
+    resp = networkTrain.predict(model, imgTG)
+    img = cv2.imread('Traindata/' + chr(int(resp[1] + 64)) + '_2.jpg')
+    if not img.any():
+        print("No data received from dataset")
+    return img, chr(int(resp[1] + 64))
